f59ce131fd579bf98fa7afff0c1a03f1f24a64f1
Bizimana Musa <bizimanamusa12@gmailcom>
2024-10-24 11:48:08 -0500

    initial

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.test;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        //TextView to display "Hello, World!"
+        TextView textView = new TextView(this);
+        textView.setText("Hello, World!");
+        textView.setTextSize(24);
+
+        setContentView(textView);
+    }
+
+}
